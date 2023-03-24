ad22aca6353c450e3bb81bd4111286d532b8fafc
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-03-24 09:15:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const person = {
     };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+var team= ['football', 'cricket', 'vollyball', 'hockey'];
+var team1=players[];
+
+var cap1=person;
 
 
 export {players,person,team,team1,cap1}
